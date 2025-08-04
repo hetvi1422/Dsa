a9859f2fc7880cbd965d8d0a265f8349e108c419
hetvi1422 <belanihetvi@gmail.com>
2025-08-04 21:24:18 +0530

    Add files via upload

---
+++ 4. C++ Ends Here/armstrong.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<math.h>
+
+
+using namespace std;
+
+int count_digit(int num)
+{
+    int count=0;
+    while (num)
+    {
+        count++;
+        num/=10;
+    }
+    return count;
+    
+}
+
+
+bool armstrong(int num, int digit)
+{
+    int original_num=num,rem, ans=0;
+
+    while (num)
+    {
+        rem = num%10;
+        num/=10;
+        ans = ans+pow(rem,digit);
+
+    }
+    if(original_num==ans)
+    return 1;
+    else
+    return 0;
+}
+
+
+
+int main()
+{
+    int num;
+    cout<<"enter the one number: ";
+    cin>>num;
+//count digit
+    int digit=count_digit(num);
+//armstrong num
+    cout<<armstrong(num,digit)<<endl;
+    return 0;
+}
